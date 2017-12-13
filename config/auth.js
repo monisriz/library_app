51b98b09c33e9df4ef9ad455e83b36426b09d9d3
Monis Rizvi <monisriz@gmail.com>
2017-12-13 10:28:43 -0600

    update domain

--- config/auth.js
+++ config/auth.js
@@ -3,22 +3,11 @@
 // expose our config directly to our application using module.exports
 module.exports = {
 
-    // 'facebookAuth' : {
-    //     'clientID'      : 'your-secret-clientID-here', // your App ID
-    //     'clientSecret'  : 'your-client-secret-here', // your App Secret
-    //     'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
-    // },
-    //
-    // 'twitterAuth' : {
-    //     'consumerKey'       : 'your-consumer-key-here',
-    //     'consumerSecret'    : 'your-client-secret-here',
-    //     'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
-    // },
 
     'googleAuth' : {
         'clientID'      : '991666356404-iqk2feg591fig7p6qnr7l8iphvtdc5r5.apps.googleusercontent.com',
         'clientSecret'  : 'FrH0a1uEcbZm3b4s_4qO9yBD',
-        'callbackURL'   : 'http://localhost:8000/auth/google/callback'
+        'callbackURL'   : 'https://library.monisrizvi.com/auth/google/callback'
     }
 
 };
